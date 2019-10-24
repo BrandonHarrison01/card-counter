86f5e8bb697129a8a52b2de2b71ebb55a914f09c
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-10-24 17:20:33 -0600

    updated title

--- blakjak-counter/src/components/HighCards.js
+++ blakjak-counter/src/components/HighCards.js
@@ -7,7 +7,9 @@ function HighCards(props) {
             <span>10</span><span>J</span><span>Q</span><span>K</span><span>A</span>
             <div className='card-data'>
                 <h5>showing {props.highThresh}/20 cards</h5>
-                <h5 className={props.high >= 50 ? 'green' : props.high >= 20 ? 'yellow' : 'red'} >{ props.deck === 0 ? props.deck : props.high}% chance of flipping</h5>
+                <h5 className={props.high >= 50 ? 'green' : props.high >= 20 ? 'yellow' : 'red'} >
+                    { props.deck === 0 ? props.deck : props.high}% chance of flipping
+                </h5>
             </div>
         </div>
     )
--- blakjak-counter/src/components/MedCards.js
+++ blakjak-counter/src/components/MedCards.js
@@ -7,7 +7,9 @@ function MedCards(props) {
             <span>7</span><span>8</span><span>9</span>
             <div className='card-data'>
                 <h5>showing {props.medThresh}/12 cards</h5>
-                <h5 className={props.med >= 50 ? 'green' : props.med >= 15 ? 'yellow' : 'red'} >{ props.deck === 0 ? props.deck : props.med}% chance of flipping</h5>
+                <h5 className={props.med >= 50 ? 'green' : props.med >= 15 ? 'yellow' : 'red'} >
+                    { props.deck === 0 ? props.deck : props.med}% chance of flipping
+                </h5>
             </div>
         </div>
     )
